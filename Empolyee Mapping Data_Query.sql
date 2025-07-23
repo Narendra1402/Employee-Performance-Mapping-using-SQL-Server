341c081a81d484ff1f041f5e6517e458f1ef3ac6
Narendra Singh <122566558+Narendra1402@users.noreply.github.com>
2025-07-23 15:54:59 +0530

    Add files via upload

--- Empolyee Mapping Data_Query.sql
+++ Empolyee Mapping Data_Query.sql
@@ -1,17 +1,14 @@
-﻿3.Write a query to fetch EMP_ID, FIRST_NAME, LAST_NAME, GENDER, and DEPARTMENT from the 
-employee record table, and make a list of employees and details of their department.
-
+﻿--1.Write a query to fetch EMP_ID, FIRST_NAME, LAST_NAME, GENDER, and DEPARTMENT from the employee record table, and make a list of employees and details of their department.
 
 SELECT
 EMP_ID,FIRST_NAME,LAST_NAME,GENDER,DEPT
 FROM employeeRecord
 
 
-4. Write a query to fetch EMP_ID, FIRST_NAME, LAST_NAME, GENDER, DEPARTMENT, and 
-EMP_RATING if the EMP_RATING is:  
- less than two 
- greater than four  
- between two and four
+--2. Write a query to fetch EMP_ID, FIRST_NAME, LAST_NAME, GENDER, DEPARTMENT, and EMP_RATING if the EMP_RATING is:  
+-- less than two 
+-- greater than four  
+-- between two and four
 
 SELECT
 EMP_ID,FIRST_NAME,LAST_NAME,
@@ -23,8 +20,7 @@ EMP_RATING BETWEEN 2 AND 4
 ORDER BY EMP_RATING ASC
 
 
-5. Write a query to concatenate the FIRST_NAME and the LAST_NAME of employees in the Finance 
-department from the employee table and then give the resultant column alias as NAME.
+--3. Write a query to concatenate the FIRST_NAME and the LAST_NAME of employees in the Finance department from the employee table and then give the resultant column alias as NAME.
 
 
 SELECT 
@@ -34,8 +30,7 @@ WHERE DEPT = 'Finance';
 
 
 
-6.Write a query to list only those employees who have someone reporting to them. Also, show the number of 
-reporters (including the President). 
+--4.Write a query to list only those employees who have someone reporting to them. Also, show the number of reporters (including the President). 
 
 SELECT 
 e.EMP_ID,e.FIRST_NAME,e.LAST_NAME,e.ROLE,
@@ -46,8 +41,7 @@ ON e.EMP_ID = r.MANAGER_ID
 GROUP BY e.EMP_ID, e.FIRST_NAME,e.ROLE, e.LAST_NAME
 ORDER BY Num_Reporters DESC
 
-7. Write a query to list down all the employees from the healthcare and finance departments using union. 
-Take data from the employee record table. 
+--5. Write a query to list down all the employees from the healthcare and finance departments using union. Take data from the employee record table. 
 
 SELECT 
 EMP_ID, FIRST_NAME, 
@@ -61,9 +55,7 @@ LAST_NAME, GENDER,DEPT
 FROM employeeRecord
 WHERE DEPT ='Finance'
 
-8.Write a query to list down employee details such as EMP_ID, FIRST_NAME, LAST_NAME, ROLE, 
-DEPARTMENT, and EMP_RATING grouped by dept. Also, include the respective employee rating along 
-with the max emp rating for the department.
+--6.Write a query to list down employee details such as EMP_ID, FIRST_NAME, LAST_NAME, ROLE, DEPARTMENT, and EMP_RATING grouped by dept. Also, include the respective employee rating along with the max emp rating for the department.
 
 SELECT
 EMP_ID, FIRST_NAME, LAST_NAME, ROLE, DEPT, EMP_RATING, 
@@ -71,8 +63,7 @@ MAX(EMP_RATING) OVER(PARTITION BY DEPT) AS [Avg Dept Rating]
 FROM employeeRecord
 
 
-9. Write a query to calculate the minimum and the maximum salary of the employees in each role. Take data 
-from the employee record table.
+--7. Write a query to calculate the minimum and the maximum salary of the employees in each role. Take data from the employee record table.
 
 SELECT 
 ROLE, MIN(SALARY) AS [Min Salary], MAX(SALARY) AS [Max Salary]
@@ -80,15 +71,13 @@ FROM employeeRecord
 GROUP BY ROLE
 
 
-10. Write a query to assign ranks to each employee based on their experience. Take data from the employee 
-record table.
+--8. Write a query to assign ranks to each employee based on their experience. Take data from the employee record table.
 
 SELECT EMP_ID, FIRST_NAME, LAST_NAME, ROLE, DEPT, EMP_RATING,
 EXP,RANK() OVER(ORDER BY EXP DESC) AS RANK_EXP
 FROM employeeRecord
 
-11. Write a query to create a view that displays employees in various countries whose salary is more than six 
-thousand. Take data from the employee record table. 
+--9. Write a query to create a view that displays employees in various countries whose salary is more than six thousand. Take data from the employee record table. 
 
 SELECT 
 EMP_ID, FIRST_NAME, LAST_NAME, COUNTRY, SALARY
@@ -97,8 +86,7 @@ WHERE SALARY > 6000
 ORDER BY SALARY
 
 
-12. Write a nested query to find employees with experience of more than ten years. Take data from the 
-employee record table. 
+--10. Write a nested query to find employees with experience of more than ten years. Take data from the employee record table. 
 
 SELECT
 EMP_ID, FIRST_NAME, LAST_NAME, EXP
@@ -107,8 +95,7 @@ WHERE EXP IN (SELECT EXP
 FROM employeeRecord
 WHERE EXP > 10)
 
-13. Write a query to create a stored procedure to retrieve the details of the employees whose experience is 
-more than three years. Take data from the employee record table.
+--11. Write a query to create a stored procedure to retrieve the details of the employees whose experience is more than three years. Take data from the employee record table.
 
 
 CREATE PROCEDURE ThreeExpEmployees
@@ -123,15 +110,14 @@ END;
 EXEC ThreeExpEmployees;
 
 
-14.Write a query using stored functions in the project table to check whether the job profile assigned to each 
-employee in the data science team matches the organization’s set standard. 
+--12.Write a query using stored functions in the project table to check whether the job profile assigned to each employee in the data science team matches the organization’s set standard. 
 
-The standard being: 
-For an employee with experience less than or equal to 2 years assign 'JUNIOR DATA SCIENTIST', 
-For an employee with the experience of 2 to 5 years assign 'ASSOCIATE DATA SCIENTIST', 
-For an employee with the experience of 5 to 10 years assign 'SENIOR DATA SCIENTIST', 
-For an employee with the experience of 10 to 12 years assign 'LEAD DATA SCIENTIST', 
-For an employee with the experience of 12 to 16 years assign 'MANAGER'.
+--The standard being: 
+--For an employee with experience less than or equal to 2 years assign 'JUNIOR DATA SCIENTIST', 
+--For an employee with the experience of 2 to 5 years assign 'ASSOCIATE DATA SCIENTIST', 
+--For an employee with the experience of 5 to 10 years assign 'SENIOR DATA SCIENTIST', 
+--For an employee with the experience of 10 to 12 years assign 'LEAD DATA SCIENTIST', 
+--For an employee with the experience of 12 to 16 years assign 'MANAGER'.
 
 
 CREATE FUNCTION dbo.fn_StandardJobProfile (@exp INT)
@@ -166,8 +152,7 @@ SELECT
     dbo.fn_StandardJobProfile(EXP) AS Expected_Profile
 FROM dataScienceTeam;
 
-15. Create an index to improve the cost and performance of the query to find the employee whose 
-FIRST_NAME is ‘Eric’ in the employee table after checking the execution plan.
+--13. Create an index to improve the cost and performance of the query to find the employee whose FIRST_NAME is ‘Eric’ in the employee table after checking the execution plan.
 
 
 
@@ -192,8 +177,7 @@ SET STATISTICS TIME OFF;
 
 
 
-16. Write a query to calculate the bonus for all the employees, based on their ratings and salaries (Use the 
-formula: 5% of salary * employee rating). 
+--14. Write a query to calculate the bonus for all the employees, based on their ratings and salaries (Use the formula: 5% of salary * employee rating). 
 
 
 SELECT
@@ -201,7 +185,7 @@ EMP_ID,SALARY,EMP_RATING,
 (SALARY *5/100)*EMP_RATING AS BONUS
 FROM employeeRecord
 
-17.Write a query to display employee names along with the names of the projects they are assigned to.
+--15.Write a query to display employee names along with the names of the projects they are assigned to.
 
 SELECT 
 e.EMP_ID,e.FIRST_NAME,e.LAST_NAME,p.PROJ_NAME
@@ -209,7 +193,7 @@ FROM employeeRecord e
 INNER JOIN projectTable p
 ON e.PROJ_ID = p.PROJECT_ID;
 
-Q18: .List employees,those who are not assigned to any project.
+--16: .List employees,those who are not assigned to any project.
 
 SELECT 
     e.EMP_ID,e.FIRST_NAME,e.LAST_NAME,p.PROJ_NAME
@@ -219,7 +203,7 @@ ON e.PROJ_ID = p.PROJECT_ID
 WHERE p.PROJ_NAME IS NULL
 
 
-Q19.List employee names, their project names, and their managers' names.
+--17.List employee names, their project names, and their managers' names.
 
 SELECT 
     e.FIRST_NAME AS Employee,
@@ -231,7 +215,7 @@ JOIN projectTable p
 LEFT JOIN employeeRecord m
     ON e.MANAGER_ID = m.EMP_ID;
 
- Q20.Show employees who manage others but also report to another manager.
+--18.Show employees who manage others but also report to another manager.
 
 SELECT DISTINCT m.EMP_ID, m.FIRST_NAME, m.LAST_NAME
 FROM employeeRecord e
@@ -239,7 +223,7 @@ JOIN employeeRecord m ON e.MANAGER_ID = m.EMP_ID
 WHERE m.MANAGER_ID IS NOT NULL;
 
 
- Q21.List projects that have only one employee assigned.
+--19.List projects that have only one employee assigned.
 
 SELECT p.PROJ_NAME, COUNT(e.EMP_ID) AS Assigned_Employees
 FROM projectTable p
@@ -250,7 +234,7 @@ HAVING COUNT(e.EMP_ID) = 1;
 
 
 
-Q22 Find top 3 highest paid employees per department
+--20 Find top 3 highest paid employees per department
 
 WITH RankedSalaries AS (
     SELECT *,
@@ -265,4 +249,4 @@ ORDER BY DEPT ASC
 
 
 
-EXEC sp_help 'employeeRecord';
+
